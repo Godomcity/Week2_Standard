eb107a496608dd6718a09f02744ecf4010f29950
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-10-22 19:49:36 +0900

    Q1-3

--- Assets/Scripts/Manager/QuestManager.cs
+++ Assets/Scripts/Manager/QuestManager.cs
@@ -6,17 +6,29 @@ public class QuestManager : MonoBehaviour
 {
     private static QuestManager instance;
 
-    public static QuestManager Instance 
-    { 
-        get 
+    public static QuestManager Instance
+    {
+        get
         {
             if (instance == null)
             {
                 FindObjectOfType<QuestManager>();
                 new GameObject().AddComponent<QuestManager>();
             }
-            return instance; 
+            return instance;
         }
         set { instance = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
