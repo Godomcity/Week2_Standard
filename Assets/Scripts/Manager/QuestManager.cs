2c866d45917f642378ec11aa14a47790c30fa1b3
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-10-22 19:45:43 +0900

    [Update] Property Singleton
    프로퍼티를 활용한 Get/Set

--- Assets/Scripts/Manager/QuestManager.cs
+++ Assets/Scripts/Manager/QuestManager.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
     private static QuestManager instance;
+
+    public static QuestManager Instance 
+    { 
+        get 
+        {
+            if (instance == null)
+            {
+                FindObjectOfType<QuestManager>();
+                new GameObject().AddComponent<QuestManager>();
+            }
+            return instance; 
+        }
+        set { instance = value; }
+    }
 }
