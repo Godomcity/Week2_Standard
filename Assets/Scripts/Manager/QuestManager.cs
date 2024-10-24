a09b68a47ecbee87d897e88c2df1eb6dec14539e
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-10-24 12:46:18 +0900

    Q3 - Select

--- Assets/Scripts/Manager/QuestManager.cs
+++ Assets/Scripts/Manager/QuestManager.cs
@@ -6,7 +6,9 @@ public class QuestManager : MonoBehaviour
 {
     private static QuestManager instance;
 
-    public DataSO[] quests; 
+    public DataSO[] quests;
+    public MonsterQuestDataSO[] monsters;
+    public EncounterQuestDataSO[] encounters;
 
     public static QuestManager Instance
     {
@@ -36,7 +38,11 @@ public class QuestManager : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log($"Quest 1 - {quests[0].QuestName} (최소 레벨 {quests[0].QuestRequiredLevel})");
-        Debug.Log($"Quest 2 - {quests[1].QuestName} (최소 레벨 {quests[1].QuestRequiredLevel})");
+        //Debug.Log($"Quest 1 - {quests[0].QuestName} (최소 레벨 {quests[0].QuestRequiredLevel})");
+        Debug.Log($"Quest 1 - {monsters[0].QuestName} (최소 레벨 {monsters[0].QuestRequiredLevel})" +
+            $" {monsters[0].MonsterName}를 {monsters[0].Monster}마리 소탕");
+        //Debug.Log($"Quest 2 - {quests[1].QuestName} (최소 레벨 {quests[1].QuestRequiredLevel})");
+        Debug.Log($"Quest 2 - {encounters[0].QuestName} (최소 레벨 {encounters[0].QuestRequiredLevel})" +
+            $" {encounters[0].npcName}과 대화하기");
     }
 }
